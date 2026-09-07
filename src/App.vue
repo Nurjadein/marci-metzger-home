765bf667a5b87e2900495f96962574d84180f572
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-09-07 11:24:34 +0800

    add all sections

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>Hello World Some changes</h1>
-</template>
+  <div class="min-h-screen bg-white text-slate-900">
+    <SiteNavbar />
 
-<style scoped></style>
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <GetItSoldSection />
+      <PropertiesSection />
+      <AffiliationSection />
+      <GallerySection />
+      <ServicesSection />
+      <SocialMediaSection />
+      <ContactSection />
+      <MapSection />
+    </main>
+
+    <SiteFooter />
+
+  </div>
+</template>
+<script setup lang="ts">
+import SiteNavbar from './components/home/SiteNavbar.vue'
+import HeroSection from './components/home/HeroSection.vue'
+import SiteFooter from './components/home/SiteFooter.vue'
+import AboutSection from './components/home/AboutSection.vue'
+import GetItSoldSection from './components/home/GetItSoldSection.vue'
+import PropertiesSection from './components/home/PropertiesSection.vue'
+import AffiliationSection from './components/home/AffiliationSection.vue'
+import GallerySection from './components/home/GallerySection.vue'
+import ServicesSection from './components/home/ServicesSection.vue'
+import SocialMediaSection from './components/home/SocialMediaSection.vue'
+import ContactSection from './components/home/ContactSection.vue'
+import MapSection from './components/home/MapSection.vue'
+
+</script>
